5d017aa0577e9840386048c8d773a3248abac6b2
Guillaume de Moffarts <guillaumedemoff@gmail.com>
2020-11-12 15:49:44 +0100

    Initial commit

---
+++ apollo.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'my-app',
+      // URL to the GraphQL API
+      url: 'http://127.0.0.1:8000/graphql',
+    },
+    // Files processed by the extension
+    includes: ['./**/*.vue'],
+  },
+}
