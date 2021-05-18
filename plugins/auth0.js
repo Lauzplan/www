fbf48a93d59d414ec5a4a54b6443609f2b2a3e95
Guillaume de Moffarts <guillaumedemoff@gmail.com>
2021-05-18 10:39:57 +0200

    Clean up file

--- plugins/auth0.js
+++ plugins/auth0.js
@@ -1,26 +1,3 @@
-// import Vue from 'vue'
-// import { Auth0Plugin } from '~/auth'
-
-// const domain = 'lauzplan.eu.auth0.com'
-// const clientId = 'K3Dj19AQ3Q7WAUz5kIv0UfSut7ccRkO0'
-
-// export default (context) => {
-//   const {
-//     app: { router },
-//   } = context
-//   Vue.use(Auth0Plugin, {
-//     domain,
-//     clientId,
-//     onRedirectCallback: (appState) => {
-//       router.push(
-//         appState && appState.targetUrl
-//           ? appState.targetUrl
-//           : window.location.pathname
-//       )
-//     },
-//   })
-// }
-
 import createAuth0Client from '@auth0/auth0-spa-js'
 
 export default async function (ctx, inject) {
